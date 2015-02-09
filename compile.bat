27766545e1dd26c3538256a24b188931b56f3e07
Corey <crm1994@gmail.com>
2015-02-09 12:59:57 -0600

    compile.bat: correct error in commented out lines

--- compile.bat
+++ compile.bat
@@ -3,5 +3,5 @@ javac -d bin/ *.java
 cd bin
 jar -cfe mccandlessMobileNode.jar MobileNode ..\MobileNode.java MobileNode.class ..\Frame.java Frame.class
 jar -cfe mccandlessForeignAgent.jar ForeignAgent ..\ForeignAgent.java ForeignAgent.class ..\Frame.java Frame.class
-@REM jar -cfe bin/millsHomeAgent.jar HomeAgent HomeAgent.java HomeAgent.class Frame.java Frame.class
-@REM jar -cfe bin/milssCorrespondent.jar Correspondent Correspondent.java Correspondent.class Frame.java Frame.class
+@REM jar -cfe bin/millsHomeAgent.jar HomeAgent ..\HomeAgent.java HomeAgent.class Frame.java ..\Frame.class
+@REM jar -cfe bin/milssCorrespondent.jar Correspondent ..\Correspondent.java Correspondent.class ..\Frame.java Frame.class

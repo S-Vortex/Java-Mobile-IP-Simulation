f61668a3e05565642ac73cfee676f08da5692c24
Corey <crm1994@gmail.com>
2015-02-09 12:58:02 -0600

    compile.bat: all binary files created in bin/

--- compile.bat
+++ compile.bat
@@ -1,5 +1,7 @@
-javac *.java
-jar -cfe mccandlessMobileNode.jar MobileNode MobileNode.java MobileNode.class Frame.java Frame.class
-jar -cfe mccandlessForeignAgent.jar ForeignAgent ForeignAgent.java ForeignAgent.class Frame.java Frame.class
-@REM jar -cfe millsHomeAgent.jar HomeAgent HomeAgent.java HomeAgent.class Frame.java Frame.class
-@REM jar -cfe milssCorrespondent.jar Correspondent Correspondent.java Correspondent.class Frame.java Frame.class
+mkdir bin
+javac -d bin/ *.java
+cd bin
+jar -cfe mccandlessMobileNode.jar MobileNode ..\MobileNode.java MobileNode.class ..\Frame.java Frame.class
+jar -cfe mccandlessForeignAgent.jar ForeignAgent ..\ForeignAgent.java ForeignAgent.class ..\Frame.java Frame.class
+@REM jar -cfe bin/millsHomeAgent.jar HomeAgent HomeAgent.java HomeAgent.class Frame.java Frame.class
+@REM jar -cfe bin/milssCorrespondent.jar Correspondent Correspondent.java Correspondent.class Frame.java Frame.class
